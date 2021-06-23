a3639012cf61c5c67f7d22c2704b3440b8f05561
Daniel Correia <dan.reia@gmail.com>
2021-06-23 20:06:16 +0200

    Data Structure Progress

--- src/data.rs
+++ src/data.rs
@@ -1,19 +1,116 @@
+/// The purpose of the data module is to implement the serialization and deserialization of objects
+/// for storage.
+///
+/// Bujo Object
+///     - content_type: "task", "note", "event"
+///     - content: the actual text
+///     - id: identifier to be used for reference in the cli
+///     - uuid: unique identifier
+///
+/// Ideally this all sits in a Data struct with a HashMap for content:
+///     Data.content:
+///         - key: id
+///         - value: BujoObject
+/// and methods for serialize and deserialize etc.
+///
+/// To prevent the data from becoming too large, all completed tasks should go into a separate data
+/// file. //TODO
+use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
 
-pub struct Data{
-    pub data: HashMap<String,i32>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
+    pub content: HashMap<i64, BujoObject>,
 }
 
-impl Data{
-    fn new()-> Data{
-        let data_temp = HashMap::new();
-        Data{
-            data : data_temp,
+impl Data {
+    /// Constructor method for Data
+    pub fn new() -> Data {
+        let data_dict = Data {
+            content: HashMap::new(),
+        };
+        data_dict
+    }
+
+    /// Method to read data from file. Used at the start of every call to bujo.
+    /// The method always reads a file called data.json in the directory specified in the .bujorc.
+    /// If the .bujo_data dir does not exist yet, it will be created.
+    pub fn read(bujo_data_dir: &PathBuf) -> Data {
+        let data_file: PathBuf = [bujo_data_dir.to_str().unwrap(), "data.json"]
+            .iter()
+            .collect();
+        let open_file = match fs::File::open(&data_file) {
+            Ok(file) => file,
+            Err(_) => {
+                let data = Data::new();
+                if bujo_data_dir.exists() == false {
+                    fs::create_dir(&bujo_data_dir).unwrap();
+                }
+                data.write(&bujo_data_dir);
+                let file = fs::File::open(&data_file).unwrap();
+                file
+            }
+        };
+        let read_data: Data = serde_json::from_reader(open_file).unwrap();
+        read_data
+    }
+
+    /// Write the data back to disk. Used at the end of every CLI call.
+    pub fn write(&self, bujo_data_dir: &PathBuf) {
+        let data_file: PathBuf = [bujo_data_dir.to_str().unwrap(), "data.json"]
+            .iter()
+            .collect();
+        let json_string = serde_json::to_string(&self).unwrap();
+        fs::write(data_file, json_string).unwrap();
+    }
+
+    /// Get the largest key value so that any new entry does not overwrite any
+    fn get_max_key(&self) -> i64 {
+        match self.content.keys().max() {
+            Some(x) => *x,
+            None => 0,
         }
+    }
+
+    /// Add an object to the Data HashMap. The idea is to only provide what is needed each time.
+    /// This will need to be modified alot going forward
+    pub fn add_object(&mut self, content_temp: String, content_type_temp: String) {
+        let key = self.get_max_key() + 1;
+        let obj = BujoObject {
+            content: content_temp,
+            content_type: content_type_temp,
+            signifier: String::from("."),
+        };
+        self.content.insert(key, obj);
+        println!("Content:{:?}\n", self.content)
+    }
+
+    /// Current a method called raw_delete was added purely to delete entries from the HashMap
+    /// directly. In the future this will need some sort of more complex structure for improved
+    /// usage.
+    pub fn delete_object(&mut self, id: &i64) {
+        self.content.remove(id);
     }
 }
 
-fn main(){
-    println!("Hi!");
+/// This is the main object template that will be extended for every entry in the Data HashMap.
+/// The idea would be that more and more attributes are added as needed to identify what the object
+/// is and where it is in the system.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BujoObject {
+    pub content_type: String,
+    pub content: String,
+    pub signifier: String,
 }
 
+impl Default for BujoObject {
+    fn default() -> BujoObject {
+        BujoObject {
+            content_type: String::from("task"),
+            content: String::from("placeholder text"),
+            signifier: String::from("."),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, SubCommand};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
 
 mod config;
 use crate::config::Config;
+
+mod data;
+use crate::data::{BujoObject, Data};
 
 ///Main binary application for bujo CLI
 fn main() {
@@ -11,17 +14,50 @@ fn main() {
         .about(crate_description!())
         .subcommand(SubCommand::with_name("init").about("Initialize bujo app and create .bujorc"))
         .subcommand(SubCommand::with_name("clean").about("Remove .bujorc and .bujo directory"))
+        .subcommand(SubCommand::with_name("print").about("Print raw json data"))
+        .subcommand(
+            SubCommand::with_name("task")
+                .about("Task Functions")
+                .subcommand(
+                    SubCommand::with_name("add").about("Add a task").arg(
+                        Arg::with_name("task_description")
+                            .value_name("FILE")
+                            .help("Sets a custom config file")
+                            .multiple(true),
+                    ),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("raw_delete")
+                .about("Delete from raw json using HashMap id")
+                .arg(Arg::with_name("id").takes_value(true)),
+        )
         .get_matches();
-    
+
     let config: Config = Config::new();
-    println!("home: {:#?}",config.home);
-    println!("bujorc: {:#?}",config.bujorc);
+    let mut data = Data::read(&config.data_dir);
 
+    match matches.subcommand() {
+        ("init", _) => config.initialize(),
+        ("clean", _) => config.clean(),
+        ("raw_delete", Some(sub_m)) => {
+            let id: i64 = sub_m.value_of("id").unwrap().parse().unwrap();
+            data.delete_object(&id);
+        },
+        ("print", _) => {
+            println!("{:?}", data);
+        }
+        ("task", Some(sub_m)) => match sub_m.subcommand() {
+            ("add", Some(sub_m)) => {
+                let x: Vec<&str> = sub_m.values_of("task_description").unwrap().collect();
+                data.add_object(x.join(" ").to_string(), String::from("task"));
+            }
+            (_, None) => println!("No value"),
+            _ => unreachable!(),
+        },
 
-    match matches.subcommand_name() {
-        Some("init") => config.initialize(),
-        Some("clean") => config.clean(),
-        None => println!("No argument provided"),
+        (_, None) => println!("No argument provided"),
         _ => unreachable!(),
     }
+    data.write(&config.data_dir);
 }
